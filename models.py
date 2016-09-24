6ffc2658eafa43f515ad9c62aa97c010b1cbf7dc
sin90 <sin.90xx@gmail.com>
2016-09-24 13:54:12 -0500

    -Added Static folder and files associated

--- models.py
+++ models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.model):
+    user_id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+
+    def __str__(self):
+        return (self.user_id + " " + self.first_name + " | " + self.last_name + " | " + self.age + " | " + self.city + ", " + self.state)
+
+
+class Trip(models.model):
+    trip_id = models.IntegerField(primary_key=True)
+    user_id = models.IntegerField(unique=True)
+    trip_location = models.CharField(max_length=200)
+    trip_start_date = models.CharField(max_length=50)
+    trip_end_date = models.CharField(max_length=50)
+
+    def __str__(self):
+        return (self.trip_id + " " + self.user_id + " | " + self.trip_location + " | " + self.trip_start_date + " | " + self.trip_end_date)
---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'goNow'
+urlpatterns = [
+	#ex: /groupTracker
+	url(r'^$', views.index, name='index'),
+	#url(r'^(?P<group_id>[0-9]+)/$', views.details, name='details'),
+	#url(r'^(?P<pk>\d+)/submit/$', views.submit, name='submit'),
+	#url(r'^submit/(?P<pk>\d+)/$', views.submit, name='submit'),
+	#url(r'^showAll/$', views.showAll, name='showAll'),
+	#url(r'^delete/$', views.delete, name='delete'),
+	#url(r'^(?P<group_id>[0-9]+)/showAll/$', views.showAll, name='showAll'),
+]

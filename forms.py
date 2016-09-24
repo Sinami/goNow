939cfe580f63d04890b9ce1f1cd71fb4a636022f
sin90 <sin.90xx@gmail.com>
2016-09-24 17:53:41 -0500

    -Placeholders added.

--- forms.py
+++ forms.py
@@ -8,8 +8,8 @@ class UserInfoForm(forms.ModelForm):
         fields = ('first_name', 'last_name', 'age', 'city', 'state',)
 
 class TripSearchForm(forms.Form):
-    trip_location = forms.CharField(label='Where..',max_length=200)
-    trip_start_date = forms.DateField(label='When..')
+    trip_location = forms.CharField(label='Where..',max_length=200,widget=forms.TextInput(attrs={'placeholder': 'Where..'})),
+    trip_start_date = forms.DateField(label='When..',widget=forms.TextInput(attrs={'placeholder': 'Search'})),
     #trip_end_date = forms.DateField()
     trip_budget = forms.IntegerField(label='Budget..')
     distance_from_you = forms.IntegerField(label='Area Range..')

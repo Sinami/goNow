2422d92e929cd689fe967d833f6534f28dcfaa1a
sin90 <sin.90xx@gmail.com>
2016-09-24 18:08:20 -0500

    -Removed Lables. -Added placeholder to fields.

--- forms.py
+++ forms.py
@@ -11,8 +11,8 @@ class TripSearchForm(forms.Form):
     trip_location = forms.CharField(label='Where..',max_length=200,widget=forms.TextInput(attrs={'placeholder': 'Where..'}))
     trip_start_date = forms.DateField(label='When..',widget=forms.TextInput(attrs={'placeholder': 'When..'}))
     #trip_end_date = forms.DateField()
-    trip_budget = forms.IntegerField(label='Budget..')
-    distance_from_you = forms.IntegerField(label='Area Range..')
+    trip_budget = forms.IntegerField(label='Budget..',widget=forms.TextInput(attrs={'placeholder': 'Budget..'}))
+    distance_from_you = forms.IntegerField(label='Area Range..',widget=forms.TextInput(attrs={'placeholder': 'Area Range..'}))
     #class Meta:
     #    model = Trip
     #   fields = ('trip_location','trip_start_date','trip_end_date',)
